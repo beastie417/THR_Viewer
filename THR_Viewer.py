5c53cfdb9a8f7ad58a3d25abe4bee9316d0825b0
beastie417 <odritter@gmail.com>
2023-05-09 20:58:01 -0500

    Initial Animation

--- THR_Viewer.py
+++ THR_Viewer.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import tkinter as tk
 from tkinter import filedialog
+from matplotlib.animation import FuncAnimation
 
 root = tk.Tk()
 root.withdraw()
@@ -23,8 +24,21 @@ circle.set_theta_offset(np.pi/2.0)
 
 circle.plot(reader.theta, reader.rho, marker='none', linestyle='--')
 
-circle.set_xticks([])
-circle.set_yticks([])
+ticks = 100
+speedup = int(len(reader.theta)/ticks)
+
+
+def animate(i):
+    circle.clear()
+    # Plot that point using the x and y coordinates
+    circle.plot(reader.theta[0:i*speedup], reader.rho[0:i*speedup], marker='none', linestyle='--')
+    # Set the x and y axis to display a fixed range
+    circle.set_xticks([])
+    circle.set_yticks([])
+    circle.set_ylim([0, 1])
+
+ani = FuncAnimation(fig, animate, frames=ticks+30, interval=10, repeat=True)
+
 
 # display the Polar plot
 plt.show()
