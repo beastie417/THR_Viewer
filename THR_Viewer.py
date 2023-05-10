4265fc08a93a74726a0cfcf6924356f8162ed426
beastie417 <odritter@gmail.com>
2023-05-10 08:35:55 -0500

    And the change

--- THR_Viewer.py
+++ THR_Viewer.py
@@ -21,12 +21,6 @@ while True:
         fig = plt.figure()
         circle = fig.add_subplot(projection='polar')
 
-        #Make increasing angle clockwise
-        circle.set_theta_direction(-1)
-        
-        #Shift 0 deg to top of graph
-        circle.set_theta_offset(np.pi/2.0)
-
         #Open plot window maximized
         figManager = plt.get_current_fig_manager()
         figManager.window.state('zoomed')
@@ -40,10 +34,17 @@ while True:
             circle.clear()
             # Plot that point using the x and y coordinates
             circle.plot(reader.theta[0:i*speedup], reader.rho[0:i*speedup], marker='none', linestyle='--')
+            
             # Set the x and y axis to display a fixed range
             circle.set_xticks([])
             circle.set_yticks([])
             circle.set_ylim([0, 1])
+
+            #Make increasing angle clockwise
+            circle.set_theta_direction(-1)
+        
+            #Shift 0 deg to top of graph
+            circle.set_theta_offset(np.pi/2.0)
 
         ani = FuncAnimation(fig, animate, frames=ticks+30, interval=10, repeat=True)
 
